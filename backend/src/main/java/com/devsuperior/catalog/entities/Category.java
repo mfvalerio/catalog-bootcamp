14371e3f04e761aee025faae1be4bd7b73f6abc6
mfvalerio <mariana.dfvalerio@gmail.com>
2023-10-12 15:43:08 -0300

    Implementa classe e endpoint get para categorias

--- backend/src/main/java/com/devsuperior/catalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/catalog/entities/Category.java
@@ -1,2 +1,31 @@
-package com.devsuperior.catalog;public class Category {
+package com.devsuperior.catalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
--- backend/src/main/java/com/devsuperior/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/catalog/resources/CategoryResource.java
@@ -1,2 +1,24 @@
-package com.devsuperior.catalog.resources;public class CategoryResource {
+package com.devsuperior.catalog.resources;
+
+import com.devsuperior.catalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1L,"Books"));
+        categoryList.add(new Category(2L, "Electronics"));
+
+        return ResponseEntity.ok().body(categoryList);
+    }
 }
