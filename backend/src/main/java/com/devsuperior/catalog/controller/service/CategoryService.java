b3bca0bbbd348be4a64bf930d0968f0cac89dda1
mfvalerio <mariana.dfvalerio@gmail.com>
2023-10-15 18:35:58 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/catalog/controller/service/CategoryService.java
+++ backend/src/main/java/com/devsuperior/catalog/controller/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.devsuperior.catalog.domain.CategoryEntity;
 import com.devsuperior.catalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CategoryEntity> findAll() {
         return repository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO category(name) VALUES ('Books');
+INSERT INTO category(name) VALUES ('Eletronics');
+INSERT INTO category(name) VALUES ('Computers');
