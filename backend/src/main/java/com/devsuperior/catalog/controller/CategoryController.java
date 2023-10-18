8d541e3a069e7df54528ae664d40319d8b27fc0e
mfvalerio <mariana.dfvalerio@gmail.com>
2023-10-17 22:31:32 -0300

    Implementa endpoint para buscar por id

--- backend/src/main/java/com/devsuperior/catalog/controller/CategoryController.java
+++ backend/src/main/java/com/devsuperior/catalog/controller/CategoryController.java
@@ -5,13 +5,15 @@ import com.devsuperior.catalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
@@ -19,6 +21,13 @@ public class CategoryController {
 
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll() {
+
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
--- backend/src/main/java/com/devsuperior/catalog/service/CategoryService.java
+++ backend/src/main/java/com/devsuperior/catalog/service/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +23,13 @@ public class CategoryService {
 
 //        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+
+        Optional<CategoryEntity> obj = repository.findById(id);
+        CategoryEntity category = obj.get();
+        return new CategoryDTO(category);
     }
 }
